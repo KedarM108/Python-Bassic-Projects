94e1a91567c839b34c12e82bf03ebefac8bdd8ea
Kedar Mule <95762119+KedarM108@users.noreply.github.com>
2022-03-04 12:18:30 +0530

    Create Acronyms.py

---
+++ Acronyms.py
@@ -0,0 +1,15 @@
+def acronyms():
+    user_input = str(input("Enter a phrase:  "))
+    text = user_input.split()
+    a = " "
+
+    for i in text:
+        a = a + str(i[0]).upper()
+
+    print(a)
+
+
+while True:
+    acronyms()
+    if input("Do you want to continue (y/n) : ").lower().startswith("n"):
+        break
