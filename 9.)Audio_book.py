1107f82aa5736791c7a8b07b8a634ea128fcf927
Kedar Mule <95762119+KedarM108@users.noreply.github.com>
2022-03-04 12:30:04 +0530

    Create 9.)Audio_book.py

---
+++ 9.)Audio_book.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import PyPDF2
+
+book = open('elon musk.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+page = pdfReader.getPage(10)
+text = page.extractText()
+speaker.say(text)
+speaker.runAndWait()
